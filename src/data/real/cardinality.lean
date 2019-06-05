387f92c98a5d0755404d9e41513b02fba81109fe
Floris van Doorn <fpvdoorn@gmail.com>
2019-06-05 08:22:19 +0200

    move file to data/real

--- src/data/real/cardinality.lean
+++ src/data/real/cardinality.lean
@@ -3,7 +3,7 @@ Copyright (c) 2019 Floris van Doorn. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Floris van Doorn
 
-The cardinality of various number systems, like pnat, int, rat and real.
+The cardinality of the reals.
 -/
 
 import data.real.basic set_theory.ordinal analysis.specific_limits data.rat.denumerable
